95d9adbfe4b67f164c3c1b55dec2b04747dd9112
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-10-23 11:37:15 +0800

    First commit

---
+++ name_age_address.py
@@ -0,0 +1,5 @@
+name = input("What's your name?: ")
+age = input("How old are you?: ")
+address = input("Where do you live?: ")
+
+print(f"Hi, my name is {name}.\nI am {age} years old and I live in {address}.")
